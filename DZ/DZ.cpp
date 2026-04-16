24b96bf4f85ef43202c5fc6da6835606185091e0
Krusha24 <davidov2000045@gmail.com>
2026-04-16 23:03:11 +0300

    6.02

--- DZ/DZ.cpp
+++ DZ/DZ.cpp
@@ -3,92 +3,105 @@
 
 using namespace std;
 
-// Шаблонный класс Box (коробка)
+// Шаблонная функция для обмена значениями двух переменных
 template <typename T>
-class Box {
-private:
-    T item;  // Приватная переменная типа T
+void swapValues(T& a, T& b) {
+    T temp = a;
+    a = b;
+    b = temp;
+}
 
-public:
-    // Конструктор для инициализации
-    Box(const T& value) : item(value) {
-        cout << "Создана коробка с элементом!" << endl;
-    }
-
-    // Конструктор по умолчанию
-    Box() : item() {}
-
-    // Set метод - устанавливает значение
-    void setItem(const T& value) {
-        item = value;
-    }
-
-    // Get метод - возвращает значение
-    T getItem() const {
-        return item;
-    }
-
-    // Метод для вывода информации (демонстрационный)
-    void display() const {
-        cout << "Коробка содержит: " << item << endl;
-    }
-};
+// Дополнительная шаблонная функция с двумя разными шаблонными параметрами
+// (меняет местами значения разных типов - требует совместимости типов)
+template <typename T1, typename T2>
+void swapDifferentTypes(T1& a, T2& b) {
+    T1 temp = a;
+    a = static_cast<T1>(b);
+    b = static_cast<T2>(temp);
+}
 
 int main() {
     setlocale(LC_ALL, "Russian");
 
-    cout << "=== ДЕМОНСТРАЦИЯ РАБОТЫ ШАБЛОННОГО КЛАССА BOX ===" << endl;
+    cout << "=== ДЕМОНСТРАЦИЯ РАБОТЫ ШАБЛОННОЙ ФУНКЦИИ ОБМЕНА ===" << endl;
     cout << endl;
 
-    // 1. Коробка с типом int
-    cout << "--- Коробка с типом int ---" << endl;
-    Box<int> intBox(42);
-    intBox.display();
-    cout << "Получение значения через getItem(): " << intBox.getItem() << endl;
-    intBox.setItem(100);
-    cout << "После setItem(100): " << intBox.getItem() << endl;
+    // 1. Обмен значениями типа int
+    cout << "--- Обмен int ---" << endl;
+    int x = 10, y = 25;
+    cout << "До обмена: x = " << x << ", y = " << y << endl;
+    swapValues(x, y);
+    cout << "После обмена: x = " << x << ", y = " << y << endl;
     cout << endl;
 
-    // 2. Коробка с типом double
-    cout << "--- Коробка с типом double ---" << endl;
-    Box<double> doubleBox(3.14159);
-    doubleBox.display();
-    cout << "Получение значения через getItem(): " << doubleBox.getItem() << endl;
-    doubleBox.setItem(2.71828);
-    cout << "После setItem(2.71828): " << doubleBox.getItem() << endl;
+    // 2. Обмен значениями типа double
+    cout << "--- Обмен double ---" << endl;
+    double a = 3.14159, b = 2.71828;
+    cout << "До обмена: a = " << a << ", b = " << b << endl;
+    swapValues(a, b);
+    cout << "После обмена: a = " << a << ", b = " << b << endl;
     cout << endl;
 
-    // 3. Коробка с типом string
-    cout << "--- Коробка с типом string ---" << endl;
-    Box<string> stringBox("Привет, мир!");
-    stringBox.display();
-    cout << "Получение значения через getItem(): " << stringBox.getItem() << endl;
-    stringBox.setItem("C++ шаблоны");
-    cout << "После setItem(\"C++ шаблоны\"): " << stringBox.getItem() << endl;
+    // 3. Обмен значениями типа string
+    cout << "--- Обмен string ---" << endl;
+    string str1 = "Привет", str2 = "Мир";
+    cout << "До обмена: str1 = \"" << str1 << "\", str2 = \"" << str2 << "\"" << endl;
+    swapValues(str1, str2);
+    cout << "После обмена: str1 = \"" << str1 << "\", str2 = \"" << str2 << "\"" << endl;
     cout << endl;
 
-    // 4. Дополнительная демонстрация - создание коробки без начального значения
-    cout << "--- Коробка с типом char (создана без начального значения) ---" << endl;
-    Box<char> charBox;
-    charBox.setItem('A');
-    charBox.display();
+    // 4. Обмен значениями типа char
+    cout << "--- Обмен char ---" << endl;
+    char c1 = 'A', c2 = 'Z';
+    cout << "До обмена: c1 = " << c1 << ", c2 = " << c2 << endl;
+    swapValues(c1, c2);
+    cout << "После обмена: c1 = " << c1 << ", c2 = " << c2 << endl;
     cout << endl;
 
-    // 5. Коробка с типом float
-    cout << "--- Коробка с типом float ---" << endl;
-    Box<float> floatBox(9.99f);
-    floatBox.display();
+    // 5. Обмен значениями типа bool
+    cout << "--- Обмен bool ---" << endl;
+    bool b1 = true, b2 = false;
+    cout << "До обмена: b1 = " << (b1 ? "true" : "false") << ", b2 = " << (b2 ? "true" : "false") << endl;
+    swapValues(b1, b2);
+    cout << "После обмена: b1 = " << (b1 ? "true" : "false") << ", b2 = " << (b2 ? "true" : "false") << endl;
     cout << endl;
 
-    // 6. Демонстрация работы с несколькими коробками одновременно
-    cout << "--- Работа с несколькими коробками ---" << endl;
-    Box<int> box1(10);
-    Box<double> box2(20.5);
-    Box<string> box3("Тест");
+    // 6. Демонстрация работы с разными типами (int и double)
+    cout << "--- Обмен разными типами (int и double) ---" << endl;
+    int intValue = 42;
+    double doubleValue = 99.99;
+    cout << "До обмена: intValue = " << intValue << ", doubleValue = " << doubleValue << endl;
+    swapDifferentTypes(intValue, doubleValue);
+    cout << "После обмена: intValue = " << intValue << ", doubleValue = " << doubleValue << endl;
+    cout << "(Примечание: происходит преобразование типов)" << endl;
+    cout << endl;
 
-    cout << "box1: " << box1.getItem() << endl;
-    cout << "box2: " << box2.getItem() << endl;
-    cout << "box3: " << box3.getItem() << endl;
+    // 7. Демонстрация работы с разными типами (int и string - требует осторожности)
+    cout << "--- Обмен разными типами (int и string) ---" << endl;
+    int num = 100;
+    string text = "123";
+    cout << "До обмена: num = " << num << ", text = \"" << text << "\"" << endl;
+
+    // Преобразование string в int (если возможно)
+    // Внимание: это демонстрация, в реальном коде нужно проверять возможность преобразования
+    try {
+        int tempNum = num;
+        num = stoi(text);
+        text = to_string(tempNum);
+        cout << "После обмена: num = " << num << ", text = \"" << text << "\"" << endl;
+    }
+    catch (const invalid_argument& e) {
+        cout << "Ошибка: невозможно преобразовать строку в число!" << endl;
+    }
+    cout << endl;
+
+    // 8. Использование стандартной функции swap (для сравнения)
+    cout << "--- Сравнение с std::swap ---" << endl;
+    int p = 7, q = 14;
+    cout << "До std::swap: p = " << p << ", q = " << q << endl;
+    std::swap(p, q);
+    cout << "После std::swap: p = " << p << ", q = " << q << endl;
+    cout << "Наша функция swapValues работает аналогично!" << endl;
 
     return 0;
 }
